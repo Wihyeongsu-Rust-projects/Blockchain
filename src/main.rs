fb8498f3b2f6b38aed143ee6e5a58131b9956452
Wihyeongsu <wibrother2@gmail.com>
2025-03-19 23:23:46 +0900

    Finished #4

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 mod balances;
+mod support;
 mod system;
 mod types {
+    use crate::support;
+
     pub type AccountId = String;
     pub type Balance = u128;
     pub type BlockNumber = u32;
     pub type Nonce = u32;
+    pub type Extrinsic = support::Extrinsic<AccountId, crate::RuntimeCall>;
+    pub type Header = support::Header<BlockNumber>;
+    pub type Block = support::Block<Header, Extrinsic>;
+}
+
+use crate::support::Dispatch;
+
+pub enum RuntimeCall {
+    Balances(balances::Call<Runtime>),
 }
 
 impl system::Config for Runtime {
@@ -24,11 +36,50 @@ pub struct Runtime {
 }
 
 impl Runtime {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self {
             system: system::Pallet::new(),
             balances: balances::Pallet::new(),
         }
+    }
+
+    fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+        self.system.inc_block_number();
+
+        if (self.system.block_number() != block.header.block_number) {
+            return Err("Block number mismatch");
+        }
+
+        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+            self.system.inc_nonce(&caller);
+
+            let _ = self.dispatch(caller, call).map_err(|e| {
+                eprintln!(
+                    "Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+                    block.header.block_number, i, e
+                );
+            });
+        }
+        Ok(())
+    }
+}
+
+impl crate::support::Dispatch for Runtime {
+    type Caller = <Runtime as system::Config>::AccountId;
+    type Call = RuntimeCall;
+
+    fn dispatch(
+        &mut self,
+        caller: Self::Caller,
+        runtime_call: Self::Call,
+    ) -> support::DispatchResult {
+        match runtime_call {
+            RuntimeCall::Balances(call) => {
+                self.balances.dispatch(caller, call)?;
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -41,24 +92,52 @@ fn main() {
 
     runtime.balances.set_balance(&alice, 100);
 
-    runtime.system.inc_block_number();
+    let block_1 = types::Block {
+        header: support::Header { block_number: 1 },
+        extrinsics: vec![
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::Balances(balances::Call::Transfer {
+                    to: bob.clone(),
+                    amount: 30,
+                }),
+            },
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::Balances(balances::Call::Transfer {
+                    to: charlie.clone(),
+                    amount: 20,
+                }),
+            },
+        ],
+    };
 
-    assert_eq!(runtime.system.block_number(), 1);
+    let block_2 = types::Block {
+        header: support::Header { block_number: 2 },
+        extrinsics: vec![
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::Balances(balances::Call::Transfer {
+                    to: bob.clone(),
+                    amount: 30,
+                }),
+            },
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::Balances(balances::Call::Transfer {
+                    to: charlie.clone(),
+                    amount: 20,
+                }),
+            },
+        ],
+    };
 
-    runtime.system.inc_nonce(&alice);
-
-    let _ = runtime.balances.transfer(&alice, &bob, 30).map_err(|e| {
-        println!("Error: {:?}", e);
-    });
-
-    runtime.system.inc_nonce(&alice);
-
-    let _ = runtime
-        .balances
-        .transfer(&alice, &charlie, 20)
-        .map_err(|e| {
-            println!("Error: {:?}", e);
-        });
+    runtime
+        .execute_block(block_1)
+        .expect("Wrong block execution!");
+    runtime
+        .execute_block(block_2)
+        .expect("Wrong block execution!");
 
     println!("{:#?}", runtime);
 }
